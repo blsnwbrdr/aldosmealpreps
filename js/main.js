7f983437daac880a6cebbf4a2f3e283ec2e8446c
Brandon Lew <brandon.t.lew@gmail.com>
2017-03-17 21:17:04 -0700

    minor fix

--- js/main.js
+++ js/main.js
@@ -1,7 +1,34 @@
 $(document).ready(function() {
   "use strict";
   
+// PAGE SCROLLING FEATURE
+  
+  $('a.page-scroll').bind('click', function(event) {
+      var $anchor = $(this);
+      $('html, body').stop().animate({
+          scrollTop: ($($anchor.attr('href')).offset().top - 135)
+      }, 1250, 'easeInOutExpo');
+      event.preventDefault();
+  });
+  
+// TOP BOX FEATURE - HIDE OR SHOW
+  
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 200) {
+      $('.top_box').fadeIn(500);
+    } else {
+      $('.top_box').fadeOut(500);
+    }
+  });
+  
+// CLOSE THE RESPONSIVE MENU ON CLICK
+  
+  $('.navbar-collapse ul li a').click(function() {
+      $('.navbar-toggle:visible').click();
+  });
+  
 // SCROLL REVEAL ANIMATION
+  
   window.sr = ScrollReveal();
     sr.reveal('.sr_checks', {
       duration: 1000,
@@ -18,7 +45,8 @@ $(document).ready(function() {
     });
 
 // MIXITUP MENU ANIMATION
+  
     $('#menu_list').mixItUp();
   
-})(jQuery);
+});
 
